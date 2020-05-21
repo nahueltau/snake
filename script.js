3d6c2787695aaa2576f53eb1e5ca5f1fec2e39f6
nahueltau <nahue.tau@gmail.com>
2020-05-21 12:19:09 -0300

    canvas setup

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-var a = "lalala";
-console.log(a);
+//CANVAS SETUP
+var canvasContainer = document.getElementById("canvas-container");
+var canvas = document.createElement("canvas");
+if (canvasContainer) {
+    canvasContainer.appendChild(canvas);
+}
+var ctx = canvas.getContext("2d");
+canvas.width = 640;
+canvas.height = 480;
+canvas.style.backgroundColor = "#333";
+ctx.beginPath();
+ctx.arc(95, 50, 20, 0, 2 * Math.PI);
+ctx.stroke();
--- script.ts
+++ script.ts
@@ -1,2 +1,16 @@
-let a: string = "lalala"; 
-console.log(a);
+//CANVAS SETUP
+let canvasContainer: HTMLElement|null= document.getElementById("canvas-container");
+let canvas: HTMLCanvasElement= document.createElement("canvas");
+if(canvasContainer){
+    canvasContainer.appendChild(canvas);
+}
+let ctx: any = canvas.getContext("2d");
+canvas.width=640;
+canvas.height=480;
+canvas.style.backgroundColor="#333";
+
+
+
+ctx.beginPath();
+ctx.arc(95, 50, 20, 0, 2 * Math.PI);
+ctx.stroke();
