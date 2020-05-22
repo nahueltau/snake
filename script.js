96b1866afe5d72a079e188cdb6b25e1f1cbef67f
nahueltau <nahue.tau@gmail.com>
2020-05-22 13:29:16 -0300

    grow and eat functionality added

--- script.js
+++ script.js
@@ -12,13 +12,35 @@ canvas.height = canvasHeight;
 //COLOR SETUP
 var lightStyle = "#DDD";
 var darkStyle = "#333";
+var mainColorStyle = "red";
 //CONTEXT SETUP
 var ctx = canvas.getContext("2d");
 ctx.lineWidth = "0";
-//MOVEMENT SETUP
-var currentMovementDirection = "ArrowRight";
+//GENERAL SETUP
+var getCenter = function (startPos) {
+    return canvasUnit * startPos + canvasUnit / 2;
+};
+var collisionCheck = function () {
+    if (getCenter(snakeSegments[0].x) === getCenter(baitPos.x) && getCenter(snakeSegments[0].y) === getCenter(baitPos.y)) {
+        baitPos = newBait();
+        hasAte = true;
+    }
+};
+//BAIT SETUP
+var newBait = function () {
+    return {
+        x: Math.floor(Math.random() * canvasWidth / canvasUnit),
+        y: Math.floor(Math.random() * canvasHeight / canvasUnit)
+    };
+};
+var baitPos = newBait();
+//SNAKE SETUP
 var startPosX = 0;
 var startPosY = 0;
+var snakeSegments = [{ x: startPosX, y: startPosY }];
+var hasAte = false;
+//Movement
+var currentMovementDirection = "ArrowRight";
 var movement = function () {
     switch (currentMovementDirection) {
         case "ArrowRight":
@@ -34,42 +56,57 @@ var movement = function () {
             startPosY--;
             break;
     }
-    console.log(startPosX);
+};
+var isOutOfBounds = function (outOfBounds) {
+    if (outOfBounds) {
+        if (getCenter(startPosX) < 0) {
+            startPosX = canvasWidth / canvasUnit;
+        }
+        else if (getCenter(startPosX) > canvasWidth) {
+            startPosX = -1;
+        }
+        if (getCenter(startPosY) < 0) {
+            startPosY = canvasHeight / canvasUnit;
+        }
+        else if (getCenter(startPosY) > canvasHeight) {
+            startPosY = -1;
+        }
+    }
 };
 //KEYBOARD EVENTS
 var handleInput = function (event) {
     currentMovementDirection = event.key;
 };
 window.addEventListener("keydown", handleInput);
-//DRAW
+//ANIMATION SETUP
 var animation = function () {
-    //Background
+    //Background draw
     ctx.beginPath();
     ctx.fillStyle = darkStyle;
     ctx.fillRect(0, 0, canvasWidth, canvasHeight);
     ctx.stroke();
-    //Snake
-    var snakePosXcenter = canvasUnit * startPosX + canvasUnit / 2;
-    var snakePosYcenter = canvasUnit * startPosY + canvasUnit / 2;
+    //Bait
     ctx.beginPath();
-    ctx.fillStyle = lightStyle;
-    ctx.fillRect(snakePosXcenter - canvasUnit / 2, snakePosYcenter - canvasUnit / 2, canvasUnit, canvasUnit);
+    ctx.fillStyle = mainColorStyle;
+    ctx.fillRect(getCenter(baitPos.x) - canvasUnit / 2, getCenter(baitPos.y) - canvasUnit / 2, canvasUnit, canvasUnit);
     ctx.stroke();
-    var outOfBounds = snakePosXcenter < 0 || snakePosXcenter > canvasWidth || snakePosYcenter < 0 || snakePosYcenter > canvasHeight;
-    if (outOfBounds) {
-        if (snakePosXcenter < 0) {
-            startPosX = canvasWidth / canvasUnit;
-        }
-        if (snakePosXcenter > canvasWidth) {
-            startPosX = -1;
-        }
-        if (snakePosYcenter < 0) {
-            startPosY = canvasHeight / canvasUnit;
-        }
-        if (snakePosYcenter > canvasHeight) {
-            startPosY = -1;
-        }
+    //Snake length
+    collisionCheck();
+    snakeSegments.unshift({ x: startPosX, y: startPosY });
+    if (!hasAte) {
+        snakeSegments.pop();
     }
+    hasAte = false;
+    //Snake draw
+    snakeSegments.forEach(function (position) {
+        ctx.beginPath();
+        ctx.fillStyle = lightStyle;
+        ctx.fillRect(getCenter(position.x) - canvasUnit / 2, getCenter(position.y) - canvasUnit / 2, canvasUnit, canvasUnit);
+        ctx.stroke();
+    });
+    //Snake movement and out of bounds check
+    var outOfBounds = getCenter(startPosX) < 0 || getCenter(startPosX) > canvasWidth || getCenter(startPosY) < 0 || getCenter(startPosY) > canvasHeight;
+    isOutOfBounds(outOfBounds);
     movement(currentMovementDirection);
 };
 //START ANIMATION
--- script.ts
+++ script.ts
@@ -1,25 +1,47 @@
 //CANVAS SETUP
-    let canvasContainer: HTMLElement|null= document.getElementById("canvas-container");
-    let canvas: HTMLCanvasElement= document.createElement("canvas");
+    const canvasContainer: HTMLElement|null= document.getElementById("canvas-container");
+    const canvas: HTMLCanvasElement= document.createElement("canvas");
     if(canvasContainer){
         canvasContainer.appendChild(canvas);
     }
-    let canvasWidth: number = 640;
-    let canvasHeight: number = 480;
-    let canvasUnit: number = 20;
+    const canvasWidth: number = 640;
+    const canvasHeight: number = 480;
+    const canvasUnit: number = 20;
     canvas.width=canvasWidth;
     canvas.height=canvasHeight;
 //COLOR SETUP
-    let lightStyle: string = "#DDD";
-    let darkStyle: string = "#333";
+    const lightStyle: string = "#DDD";
+    const darkStyle: string = "#333";
+    const mainColorStyle: string = "red";
 //CONTEXT SETUP
-    let ctx: any = canvas.getContext("2d");
+    const ctx: any = canvas.getContext("2d");
     ctx.lineWidth = "0";
-
-//MOVEMENT SETUP
-    let currentMovementDirection: string = "ArrowRight";
+//GENERAL SETUP
+const getCenter: (startPos:number) => number = (startPos)=>{
+    return canvasUnit*startPos+canvasUnit/2;
+}
+const collisionCheck = ()=>{
+    if(getCenter(snakeSegments[0].x)===getCenter(baitPos.x)&&getCenter(snakeSegments[0].y)===getCenter(baitPos.y)){
+        baitPos = newBait();
+        hasAte = true;
+    }
+}
+//BAIT SETUP
+    const newBait: ()=>{x:number,y:number} = ()=>{
+        return {
+            x:Math.floor(Math.random()*canvasWidth/canvasUnit),
+            y:Math.floor(Math.random()*canvasHeight/canvasUnit)
+        }
+    }
+    let baitPos: {x:number,y:number} = newBait();
+//SNAKE SETUP
     let startPosX: number=0;
     let startPosY: number=0;
+    let snakeSegments= [{x: startPosX, y: startPosY}];
+    let hasAte: boolean = false;
+    
+    //Movement
+    let currentMovementDirection: string = "ArrowRight";
     const movement: (currentMovementDirection: string)=>void = ()=>{
         switch(currentMovementDirection){
             case "ArrowRight":
@@ -35,7 +57,22 @@
                 startPosY--;
                 break;
         }
-        console.log(startPosX)
+    }
+    const isOutOfBounds = (outOfBounds:boolean)=>{
+        if(outOfBounds){
+            if(getCenter(startPosX)<0){
+                startPosX = canvasWidth/canvasUnit;
+            }
+            else if(getCenter(startPosX)>canvasWidth){
+                startPosX = -1;
+            }
+            if(getCenter(startPosY)<0){
+                startPosY = canvasHeight/canvasUnit;
+            }
+            else if(getCenter(startPosY)>canvasHeight){
+                startPosY = -1;
+            }
+        }
     }
 
 //KEYBOARD EVENTS
@@ -44,38 +81,39 @@ const handleInput: (event: KeyboardEvent)=>void = (event)=>{
 }
 window.addEventListener("keydown",handleInput);
 
-//DRAW
+//ANIMATION SETUP
 const animation: ()=>void = ()=>{
-    //Background
+
+    //Background draw
     ctx.beginPath(); 
     ctx.fillStyle = darkStyle;
     ctx.fillRect(0,0,canvasWidth, canvasHeight);
     ctx.stroke();
-    //Snake
-    let snakePosXcenter: number=canvasUnit*startPosX+canvasUnit/2;
-    let snakePosYcenter: number=canvasUnit*startPosY+canvasUnit/2;
-    ctx.beginPath(); 
-    ctx.fillStyle = lightStyle;
-    ctx.fillRect(snakePosXcenter-canvasUnit/2,snakePosYcenter-canvasUnit/2,canvasUnit, canvasUnit);
-    ctx.stroke();
-
-    let outOfBounds: boolean = snakePosXcenter<0 || snakePosXcenter>canvasWidth || snakePosYcenter<0 || snakePosYcenter>canvasHeight;
-    if(outOfBounds){
-        if(snakePosXcenter<0){
-            startPosX = canvasWidth/canvasUnit;
-        }
-        if(snakePosXcenter>canvasWidth){
-            startPosX = -1;
-        }
-        if(snakePosYcenter<0){
-            startPosY = canvasHeight/canvasUnit;
-        }
-        if(snakePosYcenter>canvasHeight){
-            startPosY = -1;
-        }
+    //Bait
+        ctx.beginPath(); 
+        ctx.fillStyle = mainColorStyle;
+        ctx.fillRect(getCenter(baitPos.x)-canvasUnit/2,getCenter(baitPos.y)-canvasUnit/2,canvasUnit, canvasUnit);
+        ctx.stroke();
+    //Snake length
+    collisionCheck();
+    snakeSegments.unshift({x:startPosX,y:startPosY});
+    if(!hasAte){
+        snakeSegments.pop();
     }
+    hasAte=false;
+    //Snake draw
+    snakeSegments.forEach(position => {
+        ctx.beginPath(); 
+        ctx.fillStyle = lightStyle;
+        ctx.fillRect(getCenter(position.x)-canvasUnit/2,getCenter(position.y)-canvasUnit/2,canvasUnit, canvasUnit);
+        ctx.stroke();
+    });
+    //Snake movement and out of bounds check
+    let outOfBounds: boolean = getCenter(startPosX)<0 || getCenter(startPosX)>canvasWidth || getCenter(startPosY)<0 || getCenter(startPosY)>canvasHeight;
+    isOutOfBounds(outOfBounds); 
     movement(currentMovementDirection);
     
+   
 }
 //START ANIMATION
 setInterval(animation,200);
